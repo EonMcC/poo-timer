3da34af4ed7d1374968c7f5f6ae0a8507e8df1ca
EonMcC <iain2eyes@gmail.com>
2020-10-09 19:27:27 +0100

    Set up User interface

--- src/app/home/data-service.service.ts
+++ src/app/home/data-service.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
 import { stopData } from './home.page';
 
+export interface User {
+  id: string;
+	email?: string;
+	name?: string;
+	signupDate?: number;
+	firstPooDate?: number;
+	hourlyRate?: number;
+	totalPaid?: number;
+	currency?: string;
+	longestPooTime?: number;
+	numberOfPoos?: number;
+	totalPooTime?: number;
+	firstLogin?: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,15 +23,7 @@ export class DataServiceService {
 
   stopTime: string;
   stopTimeRaw: number;
-  user: {
-    id: string;
-    email: string;
-    signupDate: number;
-    name?: string;
-    hourlyRate?: number;
-    currency?: string;
-    totalPooTime?: number;
-  }
+  user: User;
 
   constructor() { }
 
