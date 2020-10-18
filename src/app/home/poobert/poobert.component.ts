e7dd4fcb521faa4ee54ec551622abe4be400524b
EonMcC <iain2eyes@gmail.com>
2020-10-18 05:54:49 +0100

    Dynamically set poostreak saying

--- src/app/home/poobert/poobert.component.ts
+++ src/app/home/poobert/poobert.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { DataServiceService } from '../data-service.service';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { DataServiceService, User } from '../data-service.service';
 
 @Component({
   selector: 'app-poobert',
   templateUrl: './poobert.component.html',
   styleUrls: ['./poobert.component.scss'],
 })
-export class PoobertComponent implements OnInit {
+export class PoobertComponent implements OnInit, AfterViewInit {
+
+  user: User;
+  pooStreak: number;
 
   poobertEmotion = 'normal';
-  emotions = ['shocked', 'creepy']
+  emotions = ['creepy']
   iteration = 0;
 
   constructor(
@@ -19,14 +22,30 @@ export class PoobertComponent implements OnInit {
   ngOnInit() {
     setInterval(() => {
       if (this.iteration % 2 !== 0) {
-        const index = Math.floor(Math.random() * 2);
+        const index = Math.floor(Math.random() * this.emotions.length);
         this.poobertEmotion = this.emotions[index];
         this.iteration += 1
       } else {
         this.poobertEmotion = 'normal';
         this.iteration += 1;
       }
-    }, 10000);
+    }, 500);
+
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.user = this.dataService.user;
+      console.log(this.user)
+      this.setPooStreak()
+    }, 3000)
+  }
+
+  setPooStreak() {
+    if (this.user.pooStreak && this.user.pooStreak > 1) {
+      this.pooStreak = this.user.pooStreak;
+      this.emotions.push('shocked');
+    }
   }
 
 }
