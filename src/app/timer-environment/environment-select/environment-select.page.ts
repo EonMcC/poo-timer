d1728222a3e12eb40a39131c3b01b57d347b7a88
EonMcC <iain2eyes@gmail.com>
2020-10-22 19:32:08 +0100

    Finish Environment Select page

--- src/app/timer-environment/environment-select/environment-select.page.ts
+++ src/app/timer-environment/environment-select/environment-select.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataServiceService } from 'src/app/services/data-service.service';
+import { Environment, EnvironmentStorageService } from 'src/app/services/environment-storage.service';
+import * as moment from 'moment';
+import { User, UserStorageService } from 'src/app/services/user-storage.service';
 
 @Component({
   selector: 'app-environment-select',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnvironmentSelectPage implements OnInit {
 
-  constructor() { }
+  environments: Array<Environment>;
+  user: User;
+
+  constructor(
+    private dataService: DataServiceService,
+    private environmentStorageService: EnvironmentStorageService,
+    private userStorageService: UserStorageService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.environmentStorageService.listEnvironments().then((environments) => {
+      this.environments = environments;
+    })
+    this.user = this.dataService.user;
+  }
+
+  calculateTimingSinceDate(date) {
+    return moment(date).format('MMM \'YY');
+  }
+
+  selectEnvironment(environment) {
+    this.userStorageService.getUser().then((user) => {
+      this.user = user;
+      this.user.activeEnvironmentID = environment.id;
+      this.userStorageService.updateUser(this.user);
+      this.router.navigate(['/home']);
+    })
   }
 
 }
