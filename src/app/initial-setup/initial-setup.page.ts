86749d15f9c11c1a412c5901eb411f4716a3e299
EonMcC <iain2eyes@gmail.com>
2020-10-23 06:35:34 +0100

    Sort CSS of initial setup page. Make all details required

--- src/app/initial-setup/initial-setup.page.ts
+++ src/app/initial-setup/initial-setup.page.ts
@@ -29,39 +29,43 @@ export class InitialSetupPage implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    const userName = form.value.name || 'User';
-    const signupDate = moment.now();
-    const hourlyRate = form.value.wage || null;
-    const currency = form.value.currency || null;
-
-    this.userStorageService.addUser({userName, signupDate, activeEnvironmentID: 1}).then((user) => {
-      this.dataService.user = user;
-      this.environmentStorageService.addEnvironment({
-        id: 1,
-        name: 'Poo',
-        firstTimeDate: null,
-        hourlyRate,
-        currency,
-        longestTime: 0,
-        shortestTime: 0,
-        totalTime: 0,
-        itemCount: 0,
-        totalPaid: 0,
-        lastItemID: 0,
-        lastTimeDate: null,
-        streak: null
-      }).then((env) => {
-        this.dataService.environment = env[0];
-        this.presentToast();
-        this.router.navigate(['/home'])
+    if (form.valid) {
+      const userName = form.value.name || 'User';
+      const signupDate = moment.now();
+      const hourlyRate = form.value.wage || null;
+      const currency = form.value.currency || null;
+  
+      this.userStorageService.addUser({userName, signupDate, activeEnvironmentID: 1}).then((user) => {
+        this.dataService.user = user;
+        this.environmentStorageService.addEnvironment({
+          id: 1,
+          name: 'Poo',
+          firstTimeDate: null,
+          hourlyRate,
+          currency,
+          longestTime: 0,
+          shortestTime: 0,
+          totalTime: 0,
+          itemCount: 0,
+          totalPaid: 0,
+          lastItemID: 0,
+          lastTimeDate: null,
+          streak: null
+        }).then((env) => {
+          this.dataService.environment = env[0];
+          this.presentToast('Details saved, you\'re good to go!');
+          this.router.navigate(['/home'])
+        })
       })
-    })
-    form.reset();
+      form.reset();
+    } else {
+      this.presentToast('Please fill in all information');
+    }
   }
 
-  async presentToast() {
+  async presentToast(message) {
     const toast = await this.toastController.create({
-      message: 'Details saved, you\'re good to go!',
+      message,
       position: 'top',
       duration: 2000
     });
