00ff4653c9a2428719088e94d6daffa223664ccf
EonMcC <iain2eyes@gmail.com>
2020-10-26 06:58:34 +0000

    Add a few more sayings/emotions

--- src/app/home/poobert/poobert.component.ts
+++ src/app/home/poobert/poobert.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { EnvironmentStorageService } from 'src/app/services/environment-storage.service';
 import { DataServiceService } from '../../services/data-service.service';
 
 @Component({
@@ -6,40 +7,60 @@ import { DataServiceService } from '../../services/data-service.service';
   templateUrl: './poobert.component.html',
   styleUrls: ['./poobert.component.scss'],
 })
-export class PoobertComponent implements OnInit, AfterViewInit {
+export class PoobertComponent implements AfterViewInit {
 
   // user: User;
   pooStreak: number;
 
   poobertEmotion = 'normal';
-  emotions = ['boss']
+  preTimerEmotions = ['waiting', 'tap-start', 'ima-poo']
+  emotions = ['ima-poo']
   iteration = 0;
 
   constructor(
-    private dataService: DataServiceService
+    private dataService: DataServiceService,
+    public environmentStorageService: EnvironmentStorageService
   ) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     setInterval(() => {
-      if (this.iteration % 2 !== 0) {
-        const index = Math.floor(Math.random() * this.emotions.length);
-        this.poobertEmotion = this.emotions[index];
-        this.iteration += 1
-      } else {
-        this.poobertEmotion = 'normal';
-        this.iteration += 1;
+      if (this.dataService.environment && this.dataService.environment.streak > 0 && !this.emotions.includes('streak')) {
+        this.emotions.push('streak')
+      };
+      if (this.dataService.environment && this.dataService.environment.name === 'Poo' && !this.emotions.includes('boss')) {
+        this.emotions.push('boss');
+        this.preTimerEmotions.push('boss');
       }
-    }, 10000);
+      if (this.dataService.startTime !== undefined) {
+        if (this.iteration % 2 !== 0) {
+          const index = Math.floor(Math.random() * this.emotions.length);
+          this.poobertEmotion = this.emotions[index];
+          this.iteration += 1
+        } else {
+          this.poobertEmotion = 'normal';
+          this.iteration += 1;
+        }
+      } else {
+        if (this.iteration % 2 !== 0) {
+          const index = Math.floor(Math.random() * this.preTimerEmotions.length);
+          this.poobertEmotion = this.preTimerEmotions[index];
+          this.iteration += 1
+        } else {
+          this.poobertEmotion = 'normal';
+          this.iteration += 1;
+        }
+      }
+    }, 1000);
 
   }
 
-  ngAfterViewInit() {
+  // ngAfterViewInit() {
     // setTimeout(() => {
     //   this.user = this.dataService.user;
     //   console.log(this.user)
     //   this.setPooStreak()
     // }, 3000)
-  }
+  // }
 
   setPooStreak() {
     // if (this.user.pooStreak && this.user.pooStreak > 1) {
