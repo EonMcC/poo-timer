eb9f964a443ccfc5ba64fb6d349cae4819d0e5a2
EonMcC <iain2eyes@gmail.com>
2020-11-28 18:34:38 +0000

    Display list of items with date and duration

--- src/app/home/time-list/time-list.page.ts
+++ src/app/home/time-list/time-list.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataServiceService } from 'src/app/services/data-service.service';
+import { Item, ItemStorageService } from 'src/app/services/item-storage.service';
 
 @Component({
   selector: 'app-time-list',
@@ -8,11 +10,44 @@ import { Router } from '@angular/router';
 })
 export class TimeListPage implements OnInit {
 
+  times = [];
+
   constructor(
-    private router: Router
+    private router: Router,
+    public itemStorageService: ItemStorageService,
+    private dataService: DataServiceService,
   ) { }
 
   ngOnInit() {
+    this.getTimes()
+  }
+
+  getTimes(){
+    this.itemStorageService.getItems().then((items) => {
+      console.log(items)
+      let times = items.filter((item) => {
+        return item.environmentID === this.dataService.environment.id;
+      })
+      this.formatTimes(times);
+    })
+  }
+
+  formatTimes(times) {
+    const formattedTimes = []
+    times.forEach((time) => {
+      const minutes = (time.duration / 60);
+      const seconds = (minutes % 1) * 60;
+      if (minutes < 1) {
+        const formattedDuration = seconds.toFixed(0) + ' secs'
+        formattedTimes.push({...time, formattedDuration})
+      } else {
+        const formattedDuration = minutes.toFixed(0) + ' mins ' + seconds.toFixed(0) + ' secs'
+        formattedTimes.push({...time, formattedDuration})
+      }
+      console.log('time', time)
+    })
+    this.times = formattedTimes;
+    console.log(this.times)
   }
 
   goBack(){
